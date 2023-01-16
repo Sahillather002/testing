8641ce8fe97d0f30ff8867303f93e3d8b286ed4b
sahillather002 <sahillather002@gmail.com>
2023-01-16 13:39:46 +0530

    css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
+    <>
     <div className="App">
       <Details name = "Sahil" age = "21" />
       <Details name = "Ram " age = "20" />
     </div>
+    </>
   );
 }
 
